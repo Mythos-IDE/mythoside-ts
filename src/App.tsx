77673e455ff2f6d4585be6e8339feecfba9de0c5
Barış Salih Babacan <barissalihbabacan@gmail.com>
2026-07-13 11:27:56 +0300

    feat: initial project setup with Tauri, Vite+, and Astryx IDE layout

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import EditorLayout from "./views/EditorLayout";
+
+function App() {
+  return <EditorLayout />;
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "@astryxdesign/core/reset.css";
+import "@astryxdesign/core/astryx.css";
+import "@astryxdesign/theme-neutral/theme.css";
+import "./global.css";
+import { Theme } from "@astryxdesign/core";
+import { neutralTheme } from "@astryxdesign/theme-neutral/built";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <Theme theme={neutralTheme}>
+      <App />
+    </Theme>
+  </React.StrictMode>,
+);
